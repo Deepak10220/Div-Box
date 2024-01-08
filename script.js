baaefad25d59e27cf960420063a7d39921306e5f
Deepak10220 <109463063+Deepak10220@users.noreply.github.com>
2024-01-08 13:55:46 +0530

    file added

---
+++ script.js
@@ -0,0 +1,19 @@
+let Div = document.getElementById("Div");
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+    Div.style.backgroundColor = "yellow";
+  
+    Div.style.margin = "20px";
+  
+    Div.style.padding = "10px";
+  
+    Div.style.fontSize = "18px";
+  
+    Div.style.fontWeight = "bold";
+
+    Div.style.height = "200px";
+  
+    Div.style.width = "300px";
+  });
+  
